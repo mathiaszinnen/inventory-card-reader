05d4ab1726fb9639a47a4bbdf4df18ed1111f969
Mathias Zinnen~ <mathias.zinnen@fau.de>
2024-10-01 10:22:05 +0200

    skip already processed files in OCR

--- inventory_card_reader/processors/pero_ocr_processor.py
+++ inventory_card_reader/processors/pero_ocr_processor.py
@@ -9,6 +9,7 @@ class PeroOCRProcessor():
         config.read(config_path)
 
         self.device = get_device(device_name)
+        self.output_xml_path = output_xml_path
 
         page_parser = PageParser(config,config_path=os.path.dirname(config_path), device=self.device)
         create_dir_if_not_exists(output_xml_path)
@@ -16,9 +17,13 @@ class PeroOCRProcessor():
                                      output_render_path=None, output_logit_path=None, output_alto_path=None, output_xml_path=output_xml_path,
                                      output_line_path=None)
 
-    def parse_directory(self, image_directory):
+    def parse_directory(self, image_directory, skip_processed_files=True):
         image_exts = ['.jpg'] 
         images_to_process = [fn for fn in os.listdir(image_directory) if os.path.splitext(fn)[1] in image_exts]
+        if skip_processed_files:
+            processed_ids = [os.path.splitext(img_id)[0] for img_id in os.listdir(self.output_xml_path)]
+            images_to_process = [fn for fn in images_to_process if os.path.splitext(fn)[0] not in processed_ids]
+            print(f'Skipping {len(os.listdir(image_directory)) - len(images_to_process)} already processed images.')
         images_to_process = sorted(images_to_process)
         ids_to_process = [os.path.splitext(os.path.basename(fn))[0] for fn in images_to_process]
 
